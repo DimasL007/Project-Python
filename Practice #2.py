aed8c321c24806d4ec7d595698ec0583e76b0a57
DimasL007 <dimalinuk93@gmail.com>
2024-11-09 12:31:38 +0200

    Update Practice #2.py

--- Practice #2.py
+++ Practice #2.py
@@ -16,27 +16,27 @@ lst_name_type = ['set', 'float', 'tuple', 'list', 'bool', str, int]
 lst = [name, surname, age]
 for iteml in lst:
     if type(iteml) == int:
-        lst_count_types[-1] += 1
+        lst_count_types[-1] += 1 #Збільшує останній елемент списку
     elif type(iteml) == str:
-        lst_count_types[-2] += 1
+        lst_count_types[-2] += 1 #Збільшує передостанній елемент списку
 
 for itemc in lst_count_types:
     if itemc != 0:
-        lst_notnull.append(itemc)
-    if len(lst_notnull) == 0:
+        lst_notnull.append(itemc) #Додає в список якщо елементи не дорівнюють 0
+    if len(lst_notnull) == 0: # Кількість симвлов = 0
         print('Good')
     else:
-        if itemc == max_value:
+        if itemc == max_value: #Якщо itemc дорівнює максмальному числу значення max_value (1)
             print('Not')
             break
 
-        elif itemc > max_value:
+        elif itemc > max_value: #
             max_value = itemc
 
-inn = lst_count_types.index(max_value)
+inn = lst_count_types.index(max_value) #max_value в списку inn зберігає значення
 
-print(lst_name_type[inn])
+print(lst_name_type[inn]) #значення яке було знайде на попередньому кроці
 
-for iteml in lst:
-    if type(iteml) != lst_name_type[inn]:
+for iteml in lst: #
+    if type(iteml) != lst_name_type[inn]: # item не збігається з lst_name_type[inn] то елемент видаляється з lst
         lst.remove(iteml)
