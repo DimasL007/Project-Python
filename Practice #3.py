00c4f744620631409e2bf4829ae739db8851c0a2
DimasL007 <dimalinuk93@gmail.com>
2024-12-09 13:51:30 +0200

    Update Practice #3.py

--- Practice #3.py
+++ Practice #3.py
@@ -5,20 +5,22 @@ def def_lst_unique(lst):
     lst_unique = []  # Зберігає унікальні значення
     lst_in_set = set()  # Використовується для перевірки на унікальність
     for item in lst:
-        item = item.lower() if type(item) == str else item  # Рядки переводимо в малий регістр
-        if item not in lst_in_set:  #Чи Немає item в lst_in_set
-            lst_unique.append(item)  # Додаємо його до результату
-            lst_in_set.add(item)  # Помічаємо як оброблене
-    return lst_unique
+        item = item.lower() if type(item) == str else item  # Рядки переводимо в малий регістр якщо item є str
+
+        if item not in lst_in_set: #Чи Немає item в lst_in_set
+            lst_unique.append(item)  # Додаємо унікальний елемент до списку
+            lst_in_set.add(item)  # Помічаємо цей елемент як оброблений
+        return lst_unique  # Повертаємо список без дублікатів
 
 # Функція сортування списку
 def def_lst_sort(lst):
-    lst_str = [k for k in lst if type(k) == str]  # Вибір рядків
-    lst_int = [k for k in lst if type(k) == int]  # Вибір чисел
+    lst_str = [k for k in lst if type(k) == str]  # Створюємо список для всіх елементів, які є рядками
+    lst_int = [k for k in lst if type(k) == int]  # Створюємо список для всіх елементів, які є числами (цілими числами)
+    # Сортуємо числа окремо, потім рядки, після чого об'єднуємо ці два списки
     return sorted(lst_int) + sorted(lst_str)  # Сортування чисел і рядків окремо, потім об'єднання
 
 # Видаляємо дублікати та сортуємо
 r = def_lst_unique(lst)  # Отримуємо список без дублікатів
 sorted_list = def_lst_sort(r)  # Сортуємо список
 
-print(sorted_list)  # Вивід результату
+print(sorted_list)  # Вивід Сортованого Списку
